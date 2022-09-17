4a0e3df9fda85b9de5a9a42db432afefd0828eb1
Tran Kim Hieu <trankimhieu567@gmail.com>
2022-09-17 12:38:08 +0700

    blabla

--- src/GUI/course.java
+++ src/GUI/course.java
@@ -19,6 +19,7 @@ import com.jgoodies.forms.layout.Sizes;
 import javax.swing.JButton;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
+import java.awt.GridLayout;
 
 public class course extends JFrame {
 
@@ -35,106 +36,125 @@ public class course extends JFrame {
 	 * Launch the application.
 	 */
 	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					course frame = new course();
-					frame.setVisible(true);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
+		System.out.println("abc");
+//		EventQueue.invokeLater(new Runnable() {
+//			public void run() {
+//				try {
+//					course frame = new course();
+//					frame.setVisible(true);
+//				} catch (Exception e) {
+//					e.printStackTrace();
+//				}
+//			}
+//		});
 	}
 
 	/**
 	 * Create the frame.
 	 */
-	public course() {
+	public void course() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 674, 390);
+		setBounds(100, 100, 761, 390);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 
 		setContentPane(contentPane);
-		contentPane.setLayout(null);
+		contentPane.setLayout(new GridLayout(0, 2, 0, 0));
 		
-		JLabel lblTm = new JLabel("T\u00ECm ki\u1EBFm kh\u00F3a h\u1ECDc");
-		lblTm.setBounds(5, 5, 137, 28);
-		lblTm.setFont(new Font("Tahoma", Font.PLAIN, 15));
-		contentPane.add(lblTm);
-		
-		textField = new JTextField();
-		textField.setBounds(142, 5, 206, 28);
-		contentPane.add(textField);
-		textField.setColumns(10);
-		
-		JLabel lblNewLabel = new JLabel("Th\u00EAm kh\u00F3a h\u1ECDc");
-		lblNewLabel.setBounds(358, 5, 297, 28);
-		lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
-		lblNewLabel.setFont(new Font("Tahoma", Font.PLAIN, 15));
-		contentPane.add(lblNewLabel);
-		
-		JLabel lblNewLabel_1 = new JLabel("Danh s\u00E1ch kh\u00F3a h\u1ECDc");
-		lblNewLabel_1.setBounds(108, 41, 137, 19);
-		lblNewLabel_1.setFont(new Font("Tahoma", Font.PLAIN, 15));
-		contentPane.add(lblNewLabel_1);
-		
-		JLabel lblNewLabel_2 = new JLabel("M\u00E3 kh\u00F3a h\u1ECDc");
-		lblNewLabel_2.setHorizontalAlignment(SwingConstants.RIGHT);
-		lblNewLabel_2.setFont(new Font("Tahoma", Font.PLAIN, 15));
-		lblNewLabel_2.setBounds(358, 44, 95, 24);
-		contentPane.add(lblNewLabel_2);
+		JPanel panel_1 = new JPanel();
+		contentPane.add(panel_1);
+		panel_1.setLayout(new FormLayout(new ColumnSpec[] {
+				ColumnSpec.decode("137px"),
+				ColumnSpec.decode("206px:grow"),},
+			new RowSpec[] {
+				RowSpec.decode("28px"),
+				RowSpec.decode("41px"),
+				RowSpec.decode("274px:grow"),}));
 		
 		table = new JTable();
-		table.setBounds(5, 69, 343, 274);
-		contentPane.add(table);
+		panel_1.add(table, "1, 3, 2, 1, fill, fill");
 		
-		JLabel lblNewLabel_2_1 = new JLabel("T\u00EAn kh\u00F3a h\u1ECDc");
-		lblNewLabel_2_1.setHorizontalAlignment(SwingConstants.RIGHT);
-		lblNewLabel_2_1.setFont(new Font("Tahoma", Font.PLAIN, 15));
-		lblNewLabel_2_1.setBounds(358, 78, 95, 24);
-		contentPane.add(lblNewLabel_2_1);
+		JLabel lblNewLabel_1 = new JLabel("Danh s\u00E1ch kh\u00F3a h\u1ECDc");
+		panel_1.add(lblNewLabel_1, "1, 2, 2, 1, center, fill");
+		lblNewLabel_1.setFont(new Font("Tahoma", Font.PLAIN, 15));
 		
-		JLabel lblNewLabel_2_2 = new JLabel("S\u1ED1 t\u00EDn ch\u1EC9");
-		lblNewLabel_2_2.setHorizontalAlignment(SwingConstants.RIGHT);
-		lblNewLabel_2_2.setFont(new Font("Tahoma", Font.PLAIN, 15));
-		lblNewLabel_2_2.setBounds(358, 112, 95, 24);
-		contentPane.add(lblNewLabel_2_2);
+		textField = new JTextField();
+		panel_1.add(textField, "2, 1, fill, fill");
+		textField.setColumns(10);
 		
-		JLabel lblNewLabel_2_3 = new JLabel("Khoa");
-		lblNewLabel_2_3.setHorizontalAlignment(SwingConstants.RIGHT);
-		lblNewLabel_2_3.setFont(new Font("Tahoma", Font.PLAIN, 15));
-		lblNewLabel_2_3.setBounds(358, 146, 95, 24);
-		contentPane.add(lblNewLabel_2_3);
+		JLabel lblTm = new JLabel("T\u00ECm ki\u1EBFm kh\u00F3a h\u1ECDc");
+		panel_1.add(lblTm, "1, 1, fill, fill");
+		lblTm.setFont(new Font("Tahoma", Font.PLAIN, 15));
+		
+		JPanel panel = new JPanel();
+		contentPane.add(panel);
+		panel.setLayout(new FormLayout(new ColumnSpec[] {
+				FormSpecs.UNRELATED_GAP_COLSPEC,
+				ColumnSpec.decode("95px"),
+				FormSpecs.UNRELATED_GAP_COLSPEC,
+				ColumnSpec.decode("192px:grow"),
+				FormSpecs.UNRELATED_GAP_COLSPEC,},
+			new RowSpec[] {
+				FormSpecs.UNRELATED_GAP_ROWSPEC,
+				RowSpec.decode("28px"),
+				FormSpecs.UNRELATED_GAP_ROWSPEC,
+				RowSpec.decode("27px"),
+				FormSpecs.RELATED_GAP_ROWSPEC,
+				RowSpec.decode("27px"),
+				FormSpecs.RELATED_GAP_ROWSPEC,
+				RowSpec.decode("28px"),
+				FormSpecs.RELATED_GAP_ROWSPEC,
+				RowSpec.decode("27px"),
+				FormSpecs.PARAGRAPH_GAP_ROWSPEC,
+				RowSpec.decode("28px"),}));
+		
+		JLabel lblNewLabel = new JLabel("Th\u00EAm kh\u00F3a h\u1ECDc");
+		panel.add(lblNewLabel, "2, 2, 3, 1, fill, fill");
+		lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
+		lblNewLabel.setFont(new Font("Tahoma", Font.PLAIN, 15));
 		
 		textField_1 = new JTextField();
-		textField_1.setBounds(463, 46, 192, 25);
-		contentPane.add(textField_1);
+		panel.add(textField_1, "4, 4, fill, fill");
 		textField_1.setColumns(10);
 		
+		JLabel lblNewLabel_2 = new JLabel("M\u00E3 kh\u00F3a h\u1ECDc");
+		panel.add(lblNewLabel_2, "2, 4, fill, fill");
+		lblNewLabel_2.setHorizontalAlignment(SwingConstants.RIGHT);
+		lblNewLabel_2.setFont(new Font("Tahoma", Font.PLAIN, 15));
+		
+		JLabel lblNewLabel_2_1 = new JLabel("T\u00EAn kh\u00F3a h\u1ECDc");
+		panel.add(lblNewLabel_2_1, "2, 6, fill, fill");
+		lblNewLabel_2_1.setHorizontalAlignment(SwingConstants.RIGHT);
+		lblNewLabel_2_1.setFont(new Font("Tahoma", Font.PLAIN, 15));
+		
 		textField_2 = new JTextField();
+		panel.add(textField_2, "4, 6, fill, fill");
 		textField_2.setColumns(10);
-		textField_2.setBounds(463, 80, 192, 25);
-		contentPane.add(textField_2);
 		
 		textField_3 = new JTextField();
+		panel.add(textField_3, "4, 8, fill, fill");
 		textField_3.setColumns(10);
-		textField_3.setBounds(463, 115, 192, 25);
-		contentPane.add(textField_3);
+		
+		JLabel lblNewLabel_2_2 = new JLabel("S\u1ED1 t\u00EDn ch\u1EC9");
+		panel.add(lblNewLabel_2_2, "2, 8, fill, fill");
+		lblNewLabel_2_2.setHorizontalAlignment(SwingConstants.RIGHT);
+		lblNewLabel_2_2.setFont(new Font("Tahoma", Font.PLAIN, 15));
+		
+		JLabel lblNewLabel_2_3 = new JLabel("Khoa");
+		panel.add(lblNewLabel_2_3, "2, 10, fill, fill");
+		lblNewLabel_2_3.setHorizontalAlignment(SwingConstants.RIGHT);
+		lblNewLabel_2_3.setFont(new Font("Tahoma", Font.PLAIN, 15));
 		
 		textField_4 = new JTextField();
+		panel.add(textField_4, "4, 10, fill, fill");
 		textField_4.setColumns(10);
-		textField_4.setBounds(463, 148, 192, 25);
-		contentPane.add(textField_4);
 		
 		JButton btnNewButton = new JButton("Th\u00EAm kh\u00F3a h\u1ECDc");
+		panel.add(btnNewButton, "2, 12, 3, 1, center, fill");
 		btnNewButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 			}
 		});
 		btnNewButton.setFont(new Font("Tahoma", Font.PLAIN, 15));
-		btnNewButton.setBounds(426, 188, 144, 28);
-		contentPane.add(btnNewButton);
 	}
 }
